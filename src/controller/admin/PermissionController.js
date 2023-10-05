3fe9c92aa00f6f35c850b647925ac2283e66cdc8
nadeemmansuriVH <nadeemmansuri.vhits@gmail.com>
2023-10-05 15:14:40 +0530

    Role & Permission CRUD & active deactive permission

---
+++ src/controller/admin/PermissionController.js
@@ -0,0 +1,126 @@
+import {
+    ResponseMessage,
+    StatusCodes,
+    handleErrorResponse,
+    getSingleData,
+    sendResponse,
+    dataCreate,
+    dataUpdated,
+    getAllData,
+    Permission
+} from "../../index.js";
+
+export const addEditPermission = async (req, res) => {
+    try {
+        const { permissionId, Game, RoleType } = req.body;
+        if (permissionId) {
+            const updatePermission = await dataUpdated({ _id: permissionId }, { RoleType, Game }, Permission);
+            if (updatePermission) {
+                return sendResponse(
+                    res,
+                    StatusCodes.OK,
+                    ResponseMessage.PERMISSION_UPDATED,
+                    updatePermission
+                );
+            } else {
+                return sendResponse(
+                    res,
+                    StatusCodes.BAD_REQUEST,
+                    ResponseMessage.PERMISSION_NOT_UPDATED,
+                    []
+                );
+            }
+        } else {
+            const savePermission = await dataCreate({ RoleType, Game }, Permission);
+            if (savePermission) {
+                return sendResponse(
+                    res,
+                    StatusCodes.CREATED,
+                    ResponseMessage.PERMISSION_CREATED,
+                    savePermission
+                );
+            } else {
+                return sendResponse(
+                    res,
+                    StatusCodes.BAD_REQUEST,
+                    ResponseMessage.PERMISSION_NOT_CREATED,
+                    []
+                );
+            }
+        }
+    } catch (error) {
+        return handleErrorResponse(res, error);
+    }
+};
+
+export const getAllPermission = async (req, res) => {
+    try {
+        const getAllPermission = await Permission.find({ is_deleted: 0 }).sort({ createdAt: -1 });
+        if (getAllPermission.length) {
+            return sendResponse(
+                res,
+                StatusCodes.OK,
+                ResponseMessage.PERMISSION_LIST,
+                getAllPermission
+            );
+        } else {
+            return sendResponse(
+                res,
+                StatusCodes.BAD_REQUEST,
+                ResponseMessage.PERMISSION_NOT_FOUND,
+                []
+            );
+        }
+    } catch (error) {
+        return handleErrorResponse(res, error);
+    }
+};
+
+export const permissionGetById = async (req, res) => {
+    try {
+        const getSinglePermission = await getSingleData({ _id: req.params.permissionId, is_deleted: 0 }, Permission)
+        if (getSinglePermission) {
+            return sendResponse(
+                res,
+                StatusCodes.OK,
+                ResponseMessage.PERMISSION_GET,
+                getSinglePermission
+            );
+        } else {
+            return sendResponse(
+                res,
+                StatusCodes.BAD_REQUEST,
+                ResponseMessage.PERMISSION_NOT_FOUND,
+                []
+            );
+        }
+
+    } catch (error) {
+        return handleErrorResponse(res, error);
+    }
+};
+
+export const permissionActiveDeActive = async (req, res) => {
+    try {
+        const { permissionId } = req.body;
+        const findPermission = await getSingleData({ _id: permissionId }, Permission)
+        if (findPermission) {
+            var message;
+            if (findPermission.isActive) {
+                findPermission.isActive = false;
+                message = ResponseMessage.PERMISSION_DEACTIVE
+            } else {
+                findPermission.isActive = true;
+                message = ResponseMessage.PERMISSION_ACTIVE
+            }
+            await findPermission.save();
+            return sendResponse(res, StatusCodes.OK, message, []);
+        } else {
+            return sendResponse(res, StatusCodes.BAD_REQUEST, ResponseMessage.PERMISSION_NOT_FOUND, []);
+        }
+
+    } catch (error) {
+        return handleErrorResponse(res, error);
+    }
+};
+
