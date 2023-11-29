355d28618a784b30a6396aa99f4e3e2dd531bbd4
NikitaKaranpuria <83944173+NikitaKaranpuria@users.noreply.github.com>
2023-11-29 12:49:30 +0530

    Delete src directory

--- src/routes/CommonRoutes.js
+++
@@ -1,8 +0,0 @@
-import { express, Auth, Upload, addEditBanner,deleteBanner,allBannerGet } from "../index.js"
-const commonRoutes = express.Router();
-// Banner Api for admin and user
-commonRoutes.post('/banner-add-edit', Auth, Upload, addEditBanner);
-commonRoutes.get('/banners', Auth, allBannerGet);
-commonRoutes.post('/banner-delete', Auth, deleteBanner);
-
-export { commonRoutes }
