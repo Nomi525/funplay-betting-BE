e3824042d64cf179248523faaacd1aca0537b799
harshVhits <harshp.vhits@gmail.com>
2023-12-28 20:03:21 +0530

    cronjob fixed

--- src/controller/user/NumberBettingController.js
+++ src/controller/user/NumberBettingController.js
@@ -1694,10 +1694,10 @@ function allDateStamps(game, time, type) {
   let serverTime = "+5:30"
   //main game start time gamedurationfrom
   const mainGameStartTime = moment(game.gameDurationFrom, "h:mm A").format(
-    "hh:mm"
+    "HH:mm"
   );
   //main game end time gamedurationto
-  const mainGameEndTime = moment(game.gameDurationTo, "h:mm A").format("hh:mm");
+  const mainGameEndTime = moment(game.gameDurationTo, "h:mm A").format("HH:mm");
   //main game start date gameTimeFrom
   const mainGameStartDate = moment(game.gameTimeFrom).format("YYYY-MM-DD");
   //main game end date gameTimeTo
