f52895061cb7d604272b694536990e3ee50a145d
preeti04barfa <preetibarfa.vhits@gmail.com>
2024-03-15 16:40:40 +0530

    resolve conflict

--- src/controller/user/UserController.js
+++ src/controller/user/UserController.js
@@ -2650,7 +2650,7 @@ export const findUserLooseBet = async (req, res) => {
     })
 
    let findData = [...findColorBettingUse, ...findColorBettingUserNew, ...findCardUser, ...findCardBettingUserNew, ...findCardBettingUserNew, ...findPenltybettingUser, ...findPenltybettingUserNew, ...findNumberbettingUser, ...findNumberbettingUserNew, ...findCommunitybettingUser, ...findNCommunitybettingUserNew ]
-console.log(findData,'kk');
+
 const totalBetAmount = findData.reduce((accumulator, currentValue) => accumulator + currentValue.betAmount, 0);
     return res.status(200).json({
       status: StatusCodes.OK,
